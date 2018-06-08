be62c72aea8fe7f2af472b0fd8575799032f8537
Jonathan Anderson <jonathan.anderson@attentec.se>
2018-06-08 17:24:36 +0200

    Improved colors and presentation in terminal

--- display.py
+++ display.py
@@ -18,20 +18,52 @@ def clear():
 
 def set_pixel(x, y, r, g, b):
     global window
-    color = curses.COLOR_BLACK
+    color = 1
     if r == 255 and g == 255 and b == 255:
-        color = curses.COLOR_WHITE
+        color = 2
     elif r == 255:
-        color = curses.COLOR_RED
+        color = 3
     elif g == 255:
-        color = curses.COLOR_GREEN
+        color = 4
     elif b == 255:
-        color = curses.COLOR_BLUE
-    window.addstr(y, x, "█", curses.color_pair(color))
+        color = 5
+    window.addstr(y * 3 + 1, x * 5 + 1, "█", curses.color_pair(color))
+    window.addstr(y * 3 + 1, x * 5 + 2, "█", curses.color_pair(color))
+    window.addstr(y * 3 + 1, x * 5 + 3, "█", curses.color_pair(color))
+    window.addstr(y * 3 + 1, x * 5 + 4, "█", curses.color_pair(color))
+    window.addstr(y * 3 + 2, x * 5 + 1, "█", curses.color_pair(color))
+    window.addstr(y * 3 + 2, x * 5 + 2, "█", curses.color_pair(color))
+    window.addstr(y * 3 + 2, x * 5 + 3, "█", curses.color_pair(color))
+    window.addstr(y * 3 + 2, x * 5 + 4, "█", curses.color_pair(color))
 
 def show():
     global window
+    add_border()
     window.refresh()
+
+def add_border():
+    global window
+    window.addstr(0, 0, "┌", curses.color_pair(6))
+    window.addstr(12, 0, "└", curses.color_pair(6))
+    window.addstr(0, 40, "┐", curses.color_pair(6))
+    window.addstr(12, 40, "┘", curses.color_pair(6))
+    for x in range(0,9):
+        if x != 0 and x != 8:
+            window.addstr(0, x * 5, "┬", curses.color_pair(6))
+            window.addstr(12, x * 5, "┴", curses.color_pair(6))
+        for y in range(1,12):
+            if x == 0 and y % 3 == 0:
+                window.addstr(y, x * 5, "├", curses.color_pair(6))
+            elif x == 8 and y % 3 == 0:
+                window.addstr(y, x * 5, "┤", curses.color_pair(6))
+            elif y % 3 == 0:
+                window.addstr(y, x * 5, "┼", curses.color_pair(6))
+            else:
+                window.addstr(y, x * 5, "│", curses.color_pair(6))
+    for y in range(0, 5):
+        for x in range(0, 8):
+            for l in range(1, 5):
+                window.addstr(y * 3, x * 5 + l, "─", curses.color_pair(6))
 
 def set_window(win):
     global window
@@ -39,8 +71,9 @@ def set_window(win):
     curses.start_color()
     curses.use_default_colors()
     curses.curs_set(0)
-    curses.init_pair(curses.COLOR_BLACK, curses.COLOR_BLACK, -1)
-    curses.init_pair(curses.COLOR_RED, curses.COLOR_RED, -1)
-    curses.init_pair(curses.COLOR_WHITE, curses.COLOR_WHITE, -1)
-    curses.init_pair(curses.COLOR_GREEN, curses.COLOR_GREEN, -1)
-    curses.init_pair(curses.COLOR_BLUE, curses.COLOR_BLUE, -1)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_WHITE)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_RED)
+    curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_GREEN)
+    curses.init_pair(5, curses.COLOR_BLUE, curses.COLOR_BLUE)
+    curses.init_pair(6, curses.COLOR_WHITE, curses.COLOR_BLACK)
